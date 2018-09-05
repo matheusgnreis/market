3a15169e129500fed938d72778be2735fed29b29
Talisson Trindade <talissonf@gmail.com>
2018-09-05 13:03:57 -0300

    model AppsImagens created

---
+++ src/Market/Model/AppsImagens.php
@@ -0,0 +1,16 @@
+<?php
+namespace Market\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AppsImagens extends Model
+{
+    /** db table */
+    protected $table = 'image_apps';
+    /** The attributes that are mass assignable. */
+    protected $fillable = ['id', 'app_id', 'alt', 'path_image', 'width_px', 'height_px'];
+    /** The attributes that will be hidden */
+    protected $hidden = [];
+    /** Indicates if the model should be timestamped. */
+    public $timestamps = false;
+}
