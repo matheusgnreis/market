d9e090db82e68457dff126855a9f65f76284b312
Talisson Trindade <talissonf@gmail.com>
2018-09-05 13:03:25 -0300

    model AppEvaluation created

---
+++ src/Market/Model/AppsEvaluation.php
@@ -0,0 +1,16 @@
+<?php
+namespace Market\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AppsEvaluation extends Model
+{
+    /** db table */
+    protected $table = 'apps_evaluations';
+    /** The attributes that are mass assignable. */
+    protected $fillable = ['partner_id', 'store_id', 'date_time', 'stars'];
+    /** The attributes that will be hidden */
+    protected $hidden = [];
+    /** Indicates if the model should be timestamped. */
+    public $timestamps = false;
+}
