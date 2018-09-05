d4d7b09cb2935864c64bcf13a0c8506a0796c41d
Talisson Trindade <talissonf@gmail.com>
2018-09-05 13:11:04 -0300

    created relationship between model comments and model imagens

--- src/Market/Model/Partner.php
+++ src/Market/Model/Partner.php
@@ -1,1 +1,16 @@
 <?php
+namespace Market\Model;
+
+use \Illuminate\Database\Eloquent\Model as Model;
+
+class Partner extends Model
+{
+    /** db table */
+    protected $table = 'partners';
+    /** The attributes that are mass assignable. */
+    protected $fillable = ['id', 'name', 'password_hash', 'member_since', 'avg_stars', 'evaluations', 'path_image', 'profile_json', 'credit'];
+    /** The attributes that will be hidden */
+    protected $hidden = [];
+    /** Indicates if the model should be timestamped. */
+    public $timestamps = false;
+}
