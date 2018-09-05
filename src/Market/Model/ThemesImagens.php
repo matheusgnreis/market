a4f103b162e76a213dd630f4484d1957c683fe1e
Talisson Trindade <talissonf@gmail.com>
2018-09-05 13:07:12 -0300

    model ThemesImagens created

---
+++ src/Market/Model/ThemesImagens.php
@@ -0,0 +1,16 @@
+<?php
+namespace Market\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ThemesImagens extends Model
+{
+    /** db table */
+    protected $table = 'image_themes';
+    /** The attributes that are mass assignable. */
+    protected $fillable = ['id', 'theme_id', 'alt', 'path_image', 'width_px', 'height_px'];
+    /** The attributes that will be hidden */
+    protected $hidden = [];
+    /** Indicates if the model should be timestamped. */
+    public $timestamps = false;
+}
