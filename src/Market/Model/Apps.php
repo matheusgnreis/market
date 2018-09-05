d66cafc3faf9821f436d516104fe935b8dd6897c
Talisson Trindade <talissonf@gmail.com>
2018-09-05 13:10:07 -0300

    created relationship between model comments and model imagens

--- src/Market/Model/Apps.php
+++ src/Market/Model/Apps.php
@@ -1,34 +1,56 @@
 <?php
 namespace Market\Model;
 
-class Apps extends \Illuminate\Database\Eloquent\Model
+use \Illuminate\Database\Eloquent\Model as Model;
+
+class Apps extends Model
 {
-	/** db table */
-	protected $table = 'apps';
-	/** The attributes that are mass assignable. */
+    /** db table */
+    protected $table = 'apps';
+    /** The attributes that are mass assignable. */
     protected $fillable = ['id', 'partner_id', 'title', 'slug', 'thumbnail', 'description', 'json_body', 'paid', 'version', 'version_date', 'type', 'module', 'load_events', 'script_uri', 'github_repository', 'authentication', 'auth_callback_uri', 'auth_scope', 'avg_stars', 'evaluations', 'website', 'link_video', 'plans_json', 'value_plan_basic', 'active'];
-	/** The attributes that will be hidden */
-	protected $hidden = [];
-	/** Indicates if the model should be timestamped. */
+    /** The attributes that will be hidden */
+    protected $hidden = [];
+    /** Indicates if the model should be timestamped. */
     public $timestamps = false;
-	
-	/**
-	 *  Create evaluations relationship with app
-	 *
-	 * @return void
-	 */
+    
+    /**
+     *  Create evaluations relationship with app
+     *
+     * @return void
+     */
     public function evaluations()
     {
-        return $this->hasMany(Evaluations::class);
+        return $this->hasMany(AppsEvaluation::class);
     }
-	
-	/**
-	 * Create partner relationship with app
-	 *
-	 * @return void
-	 */
+    
+    /**
+     * Create partner relationship with app
+     *
+     * @return void
+     */
     public function partner()
     {
         return $this->hasOne(Partners::class);
+    }
+    
+    /**
+     * Create comments relationship with app
+     *
+     * @return void
+     */
+    public function comments()
+    {
+        return $this->hasMany(AppsComments::class);
+    }
+
+    /**
+     * Create imagens relationship with app
+     *
+     * @return void
+     */
+    public function imagens()
+    {
+        return $this->hasMany(AppsImagens::class);
     }
 }
