a79f4457ae01b6a6ee8d2c9023de954dc09a0729
Talisson Trindade <talissonf@gmail.com>
2018-09-06 17:23:39 -0300

    default lang route defined

--- src/middleware.php
+++ src/middleware.php
@@ -1,1 +1,19 @@
 <?php
+
+$langRedirect = function ($request, $response, $next) {
+    $uri = $request->getUri();
+    $route = $request->getAttribute('route');
+    $args = $route->getArguments();
+    var_dump($args['lang']);
+
+    if (!isset($args['lang'])) {
+        return $response->withStatus(302)->withHeader('Location', '/pt_br');
+    } else {
+        if ($args['lang'] != 'pt_br' || !$argc['lang'] != 'en_us') {
+            var_dump($uri->getPath());
+            var_dump(explode('/', $uri->getPath()));
+            return $response->withStatus(302)->withHeader('Location', '/pt_br' . $uri->getPath());
+        }
+    }
+    return $next($request, $response);
+};
