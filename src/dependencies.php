60df13ae80573e6d4624d729e9b4577cfb09e0e1
Talisson Trindade <talissonf@gmail.com>
2018-09-06 11:44:28 -0300

    set twig view on app container

--- src/dependencies.php
+++ src/dependencies.php
@@ -36,6 +36,20 @@ $container = $app->getContainer();
 $container['cache'] = function () {
     return new \Slim\HttpCache\CacheProvider();
 };
+
+$container['view'] = function ($c) {
+
+    $view = new \Slim\Views\Twig(__DIR__.'/Market/View', [
+        'cache' => 'path/to/cache'
+    ]);
+    
+    // Instantiate and add Slim specific extension
+    $router = $c->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
+};
 //Erro 404
 $container['notFoundHandler'] = function ($c) {
     return function ($request, $response) use ($c) {
@@ -72,4 +86,3 @@ $container['logger'] = function ($c) {
 
     return $logger;
 };
-
