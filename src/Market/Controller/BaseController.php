7d92c073123132e08a8b89dce4e12bbfbc674045
Talisson Trindade <talissonf@gmail.com>
2018-09-06 12:02:32 -0300

    base controller created to extends twig dependency to another controllers

---
+++ src/Market/Controller/BaseController.php
@@ -0,0 +1,30 @@
+<?php
+namespace Market\Controller;
+
+use Interop\Container\ContainerInterface;
+
+abstract class BaseController
+{
+    protected $ci;
+
+    /**
+     * Controller constructor.
+     *
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface  $container)
+    {
+        $this->ci = $container;
+    }
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        if ($this->ci->has($name)) {
+            return $this->ci->get($name);
+        }
+    }
+}
