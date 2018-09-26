8f9ba71ed75fe55e88998a73c555eae777d45fb3
Talisson Trindade <talissonf@gmail.com>
2018-09-26 10:55:49 -0300

    new routes

--- src/routes.php
+++ src/routes.php
@@ -1,11 +1,22 @@
 <?php
 use Market\Controller\HomeController;
 use Market\Controller\LoginController;
-
+use Market\Controller\PartnerController;
+use Market\Controller\AppsController;
+use Market\Services\Helpers;
+use Market\Controller\MediaController;
+use Market\Controller\ThemesController;
 $routes = function () use ($langRedirect) {
 
     // dashboard
-    $this->get('/account', HomeController::class . ':dashboard');
+    $this->get('/logout', HomeController::class . ':logout');
+    $this->get('/account', HomeController::class . ':account');
+    $this->get('/account/settings', HomeController::class . ':account_settings');
+    $this->get('/account/statement', HomeController::class . ':account_statement');
+    $this->get('/account/add', HomeController::class . ':account_add');
+    $this->get('/account/edit', HomeController::class . ':account_edit');
+    $this->get('/account/edit/app/{id}', HomeController::class . ':account_edit_app');
+    $this->get('/account/wallet', HomeController::class . ':account_wallet');
 
     //app routes
     $this->get('/', function () {echo "redirect lang default.";})->add($langRedirect);
@@ -15,13 +26,28 @@ $routes = function () use ($langRedirect) {
     $this->get('/{lang}/signup', HomeController::class . ':signUp')->add($langRedirect);
     $this->get('/{lang}/register-password', HomeController::class . ':signUpPassword')->add($langRedirect);
     $this->get('/{lang}/apps/{slug}', HomeController::class . ':app')->add($langRedirect);
+    $this->get('/{lang}/author/{id}', HomeController::class . ':author')->add($langRedirect);
     $this->get('/{lang}/themes/{slug}', HomeController::class . ':theme')->add($langRedirect);
-
 
     //api
     $this->post('/ws/login', LoginController::class . ':login');
     $this->post('/ws/login/pass', LoginController::class . ':password');
     $this->post('/ws/login/verify', LoginController::class . ':verify');
+    $this->post('/ws/partner/profile/picture', PartnerController::class . ':updatePicture');
+
+    // path apps
+    $this->post('/ws/apps', AppsController::class . ':create');
+    $this->post('/ws/apps/media/{id}', MediaController::class . ':create');
+    $this->get('/ws/apps/slug/{slug}', AppsController::class . ':verifySlug');
+
+    // path themes
+    $this->post('/ws/themes', ThemesController::class . ':create');
+    $this->post('/ws/themes/media/{id}', MediaController::class . ':create');
+
+    // helper
+    $this->post('/ws/format/plans', Helpers::class . ':arrayToJson');
+    $this->post('/ws/format/scope', Helpers::class . ':validScope');
+
 };
 
 // Api Routes
