d5967581d0a2c262b547db8e4bbecffda154fdd6
Talisson Ferreira <talissonf@gmail.com>
2018-10-18 18:14:09 -0300

    Add: app endpoint

--- src/Market/Model/Apps.php
+++ src/Market/Model/Apps.php
@@ -8,7 +8,7 @@ class Apps extends Model
     /** db table */
     protected $table = 'apps';
     /** The attributes that are mass assignable. */
-    protected $fillable = ['id', 'partner_id', 'title', 'slug', 'category', 'thumbnail', 'description', 'json_body', 'paid', 'version', 'version_date', 'type', 'module', 'load_events', 'script_uri', 'github_repository', 'authentication', 'auth_callback_uri', 'auth_scope', 'avg_stars', 'evaluations', 'website', 'link_video', 'plans_json', 'value_plan_basic', 'active'];
+    protected $fillable = ['id', 'partner_id', 'title', 'slug', 'category', 'thumbnail', 'description', 'json_body', 'paid', 'version', 'version_date', 'type', 'module', 'load_events', 'script_uri', 'github_repository', 'authentication', 'auth_callback_uri', 'redirect_uri', 'auth_scope', 'avg_stars', 'evaluations', 'website', 'link_video', 'plans_json', 'value_plan_basic', 'active'];
     /** The attributes that will be hidden */
     protected $hidden = [];
     /** Indicates if the model should be timestamped. */
