a07e30fb810786d8ec642e7181f92a3f345b5785
Talisson Ferreira <talissonf@gmail.com>
2019-02-01 21:27:57 -0200

    find app by id

--- src/Market/Controller/AppsController.php
+++ src/Market/Controller/AppsController.php
@@ -24,18 +24,55 @@ class AppsController
     public function getAll($request, $response)
     {
         /*
-            metafields
-        */
+        metafields
+         */
         $this->requestHasMeta($request);
         /*
-            search
-        */
+        search
+         */
         $result = Apps::where($this->_params)
             ->offset($this->_offset)
             ->limit($this->_limit)
             ->get($this->_fields)
             ->toArray();
         return $response->withJson($this->response($result));
+    }
+
+    public function getById($request, $response, $args)
+    {
+        $query = Apps::where('id', $args['id'])->get();
+        //$query = Apps::find($args['id']);
+        $map = $query->map(function ($item) {
+            $data['app_id'] = $item->id;
+            $data['title'] = $item->title;
+            $data['slug'] = $item->slug;
+            $data['paid'] = (boolean) $item->paid;
+            $data['version'] = $item->version;
+            //$data['version_date'] = $item->version_date;
+            $data['type'] = $item->type;
+            if (!empty($item->module)) {
+                $data['module'] = $item->module;
+            }
+            if (!empty($item->load_events)) {
+                $data['load_events'] = explode(',', str_replace(["\\", "[", "]", '"'], '', (string) $item->load_events));
+            }
+            if (!empty($item->script_uri)) {
+                $data['script_uri'] = $item->script_uri;
+            }
+            if (!empty($item->redirect_uri)) {
+                $data['redirect_uri'] = $item->redirect_uri;
+            }
+            $data['github_repository'] = $item->github_repository;
+            $data['authentication'] = (boolean) $item->authentication;
+            $data['auth_callback_uri'] = $item->auth_callback_uri;
+            $data['auth_scope'] = json_decode($item->auth_scope);
+            $data['state'] = 'active';
+            $data['status'] = 'active';
+            return $data;
+        });
+
+        return $map ? $response->withJson($map[0], 200)
+        : $response->withJson([], 404);
     }
 
     public function requestHasMeta($request)
@@ -51,6 +88,10 @@ class AppsController
 
         if (isset($request->getParams()['author'])) {
             $this->_params[] = ['partner_id', $request->getParams()['author']];
+        }
+
+        if (isset($request->getParams()['slug'])) {
+            $this->_params[] = ['slug', $request->getParams()['slug']];
         }
 
         if (isset($request->getParams()['category'])) {
