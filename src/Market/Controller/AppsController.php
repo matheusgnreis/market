d4b3526b105de15e27d453a03b67c4f9e189c25d
Talisson Ferreira <talissonf@gmail.com>
2019-02-01 16:25:28 -0200

    handle and parse metafields to return all applications or applications who match with metafields.

--- src/Market/Controller/AppsController.php
+++ src/Market/Controller/AppsController.php
@@ -1,246 +1,95 @@
 <?php
 namespace Market\Controller;
 
-use Slug\Slugifier as Slugifier;
 use Market\Model\Apps;
-use Market\Services\Helpers;
 
+/**
+ * Undocumented class
+ */
 class AppsController
 {
+    private $_limit = 1000;
+    private $_offset = 0;
+    private $_fields = ["id", "title", "slug", "thumbnail", "version", "paid"];
+    private $_result = [];
+    private $_params = ['active' => 1];
+    /**
+     * Construtor
+     */
     public function __construct()
     {
         new \Market\Services\Database();
     }
 
-    public function getAll($request, $response, $args)
+    public function getAll($request, $response)
     {
-        $params = $this->getSearchParams($request);
-        $skip = $this->getSearchSkip($request);
-        $take = $this->getSearchTake($request);
-        $apps = Apps::where($params)->skip($skip)->take($take)->with('partner')->get()->toArray();
-        return (object)[
-            'apps' => $apps,
-            'total' => Apps::where($params)->count()
-        ];
+        /*
+            metafields
+        */
+        $this->requestHasMeta($request);
+        /*
+            search
+        */
+        $result = Apps::where($this->_params)
+            ->offset($this->_offset)
+            ->limit($this->_limit)
+            ->get($this->_fields)
+            ->toArray();
+        return $response->withJson($this->response($result));
     }
 
-    public function getById($request, $response, $args)
+    public function requestHasMeta($request)
     {
-        //$ret = Apps::find($args['id'])->toArray();
-        //$ret = Apps::where('id', $args['id'])->first();
-
-        return $ret ? ['app'=>$ret->toArray(), 'imagens' => array_map(function ($a) {
-            $r['path'] = $a['path_image'];
-            $r['id'] = $a['id'];
-            return $r;
-        }, $ret->imagens->toArray())] : [];
-        return $ret ? $ret : $response->withJson([], 404);
-    }
-
-    public function _byId($request, $response, $args)
-    {
-        $query = Apps::where('id', $args['id'])->get();
-        //$query = Apps::find($args['id']);
-        $map = $query->map(function ($item) {
-            $data['app_id'] = $item->id;
-            $data['title'] = $item->title;
-            $data['slug'] = $item->slug;
-            $data['paid'] = (boolean)$item->paid;
-            $data['version'] = $item->version;
-            //$data['version_date'] = $item->version_date;
-            $data['type'] = $item->type;
-            if (!empty($item->module)) {
-                $data['module'] = $item->module ;
-            }
-            if (!empty($item->load_events)) {
-                $data['load_events' ] = explode(',', str_replace(["\\","[", "]", '"'], '', (string)$item->load_events));
-            }
-            if (!empty($item->script_uri)) {
-                $data['script_uri'] = $item->script_uri;
-            }
-            if (!empty($item->redirect_uri)) {
-                $data['redirect_uri'] = $item->redirect_uri;
-            }
-            $data['github_repository'] = $item->github_repository;
-            $data['authentication'] = (boolean)$item->authentication;
-            $data['auth_callback_uri'] = $item->auth_callback_uri;
-            $data['auth_scope'] = json_decode($item->auth_scope);
-            $data['state'] = 'active';
-            $data['status'] = 'active';
-            return $data;
-        });
-
-        return $map ? $response->withJson($map[0], 200) : $response->withJson([], 404);
-    }
-
-    public function getBySlug($request, $response, $args)
-    {
-        $app = Apps::where('slug', $args['slug'])->first();
-
-        if ($app) {
-            return (object) [
-                'app' => $app->toArray(),
-                //'imagens' => $app->imagens->toArray(),
-                'imagens' => array_map(function ($a) {
-                    $r['path'] = $a['path_image'];
-                    return $r;
-                }, $app->imagens->toArray()),
-                'partner' => $app->partner->toArray(),
-                'comments' => $app->comments->toArray(),
-                'evaluations' => $app->evaluations
-            ];
-        }
-        return false;
-    }
-
-    public function getByPartnerId($request, $response, $args)
-    {
-        $id = $request->getAttribute('partner_id');
-        $ret = Apps::where('partner_id', $id)->get()->toArray();
-        return $ret ? (array)$ret : [];
-        //return $ret ? $response->withJson($ret, 200) : $response->withJson([], 404);
-    }
-
-    public function getSearchParams($request)
-    {
-        $search = [];
-        $search[] = ['active', 1];
-
+        /*
+        Possíveis parametros para busca
+         */
         if (isset($request->getParams()['filter'])) {
             if ($request->getParams()['filter'] == 'free') {
-                $search[] = ['value_plan_basic', 0];
+                $this->_params[] = ['value_plan_basic', 0];
             }
         }
 
         if (isset($request->getParams()['author'])) {
-            $search[] = ['partner_id', $request->getParams()['author']];
+            $this->_params[] = ['partner_id', $request->getParams()['author']];
         }
 
         if (isset($request->getParams()['category'])) {
             if ($request->getParams()['category'] !== 'all') {
-                $search[] = ['category', $request->getParams()['category']];
+                $this->_params[] = ['category', $request->getParams()['category']];
             }
         }
 
         if (isset($request->getParams()['title'])) {
-            $search[] = ['title','like', '%' . $request->getParams()['title'] . '%'];
+            $this->_params[] = ['title', 'like', '%' . $request->getParams()['title'] . '%'];
         }
 
-        return $search;
-    }
-
-    public function getSearchSkip($request)
-    {
-        return isset($request->getParams()['skip']) ? (int) $request->getParams()['skip'] : 0;
-    }
-
-    public function getSearchTake($request)
-    {
-        return isset($request->getParams()['take']) ? (int) $request->getParams()['take'] : 12;
-    }
-
-    public function create($request, $response, $args)
-    {
-        $body = $request->getParsedBody();
-
-        $app = Apps::create([
-                    'partner_id' => !empty($body['partner_id']) ? $body['partner_id'] : null,
-                    'title' => !empty($body['title']) ? $body['title'] : null,
-                    'slug' => !empty($body['slug']) ? $body['slug'] : null,
-                    'category' => !empty($body['category']) ? $body['category'] : null,
-                    'thumbnail' => !empty($body['thumbnail']) ? $body['thumbnail'] : null,
-                    'description' => !empty($body['description']) ? $body['description'] : null,
-                    'json_body' => !empty($body['json_body']) ? $body['json_body'] : null,
-                    'paid' => isset($body['paid']) ? $body['paid'] : null,
-                    'version' => !empty($body['version']) ? $body['version'] : null,
-                    'version_date' => !empty($body['version_date']) ? $body['version_date'] : null,
-                    'type' => !empty($body['type']) ? $body['type'] : null,
-                    'module' => !empty($body['module']) ? $body['module'] : null,
-                    'load_events' => !empty($body['load_events']) ? (string)(json_encode($body['load_events'])) : null,
-                    'script_uri' => !empty($body['script_uri']) ? $body['script_uri'] : null,
-                    'github_repository' => !empty($body['github_repository']) ? $body['github_repository'] : null,
-                    'authentication' => isset($body['authentication']) ? $body['authentication'] : null,
-                    'auth_callback_uri' => !empty($body['auth_callback_uri']) ? $body['auth_callback_uri'] : null,
-                    'redirect_uri' => !empty($body['redirect_uri']) ? $body['redirect_uri'] : null,
-                    'auth_scope' => !empty($body['auth_scope']) ? $body['auth_scope'] : null,
-                    'avg_stars' => !empty($body['avg_stars']) ? $body['avg_stars'] : null,
-                    'evaluations' => !empty($body['evaluations']) ? $body['evaluations'] : null,
-                    'website' => !empty($body['website']) ? $body['website'] : null,
-                    'link_video' => !empty($body['link_video']) ? $body['link_video'] : null,
-                    'plans_json' => !empty($body['plans_json']) ? $body['plans_json'] : null,
-                    'value_plan_basic' => !empty($body['value_plan_basic']) ? $body['value_plan_basic'] : null,
-                    'active' => !empty($body['active']) ? $body['active'] : 1
-        ]);
-
-        return $app ? $response->withJson(['status' => 201, 'message' => 'created', 'app' => $app], 201) : $response->withJson(['erro' => 'Error creating new app'], 400);
-    }
-
-    public function update($request, $response, $args)
-    {
-        if (!$args['id']) {
-            return $response->withJson(['status' => 400, 'message' => 'app_id not found'], 400);
+        /*
+        offset
+         */
+        if (isset($request->getParams()['offset'])) {
+            $this->_offset = (int) $request->getParams()['offset'];
         }
 
-        $app = Apps::find($args['id']);
-
-        if (!$app) {
-            return $response->withJson(['status' => 400, 'message' => 'app not found'], 400);
+        /*
+        limit
+         */
+        if (isset($request->getParams()['limit'])) {
+            $this->_limit = (int) $request->getParams()['limit'];
         }
-
-        $body = $request->getParsedBody();
-        $update = $app->update([
-            'partner_id' => isset($body['partner_id']) ? $body['partner_id'] : $app->partner_id,
-            'title' => isset($body['title']) ? $body['title'] : $app->title,
-            'slug' => isset($body['slug']) ? $body['slug'] : $app->slug,
-            'category' => isset($body['category']) ? $body['category'] : $app->category,
-            'thumbnail' => isset($body['thumbnail']) ? $body['thumbnail'] : $app->thumbnail,
-            'description' => isset($body['description']) ? $body['description'] : $app->description,
-            'json_body' => isset($body['json_body']) ? $body['json_body'] : $app->json_body,
-            'paid' => isset($body['paid']) ? $body['paid'] : $app->paid,
-            'version' => isset($body['version']) ? $body['version'] : $app->version,
-            'version_date' => isset($body['version_date']) ? $body['version_date'] : $app->version_date,
-            'type' => isset($body['type']) ? $body['type'] : $app->type,
-            'module' => isset($body['module']) ? $body['module'] : $app->module,
-            'load_events' => isset($body['load_events']) ? $body['load_events'] : $app->load_events,
-            'script_uri' => isset($body['script_uri']) ? $body['script_uri'] : $app->script_uri,
-            'github_repository' => isset($body['github_repository']) ? $body['github_repository'] : $app->github_repository,
-            'authentication' => isset($body['authentication']) ? $body['authentication'] : $app->authentication,
-            'auth_callback_uri' => isset($body['auth_callback_uri']) ? $body['auth_callback_uri'] : $app->auth_callback_uri,
-            'auth_scope' => isset($body['auth_scope']) ? $body['auth_scope'] : $app->auth_scope,
-            'avg_stars' => isset($body['avg_stars']) ? $body['avg_stars'] : $app->avg_stars,
-            'evaluations' => isset($body['evaluations']) ? $body['evaluations'] : $app->evaluations,
-            'website' => isset($body['website']) ? $body['website'] : $app->website,
-            'link_video' => isset($body['link_video']) ? $body['link_video'] : $app->link_video,
-            'plans_json' => isset($body['plans_json']) ? $body['plans_json'] : $app->plans_json,
-            'value_plan_basic' => isset($body['value_plan_basic']) ? $body['value_plan_basic'] : $app->value_plan_basic,
-            'active' => isset($body['active']) ? $body['active'] : $app->active
-        ]);
-
-        return $update ? $response->withJson(['status' => 201, 'message' => 'updated'], 201) : false;
     }
 
-    public function destroy($request, $response, $args)
+    public function response($result)
     {
-        return Apps::where('id', '=', $args['id'])->delete() ? $response->withJson(['success' => 'App deleted'], 204) : $response->withJson(['erro' => 'App not found'], 404);
-    }
+        return [
+            'meta' => (object) [
+                'limit' => $this->_limit,
+                'offset' => $this->_offset,
+                'sort' => [],
+                'fields' => $this->_fields,
+                'query' => (object) $this->_params,
+            ],
+            'result' => $result,
+        ];
 
-    public function verifySlug($request, $response, $args)
-    {
-        $slug = $args['slug'];
-        $app = Apps::where('slug', $slug)->first();
-
-        if ($app) {
-            return $response->withJson([
-                'status' => 409,
-                'message' => 'slug in use',
-                'suggestions' => [
-                    $slug.'-'.Helpers::randomSlug(),
-                    $slug.'-'.Helpers::randomSlug(),
-                    $slug.'-'.Helpers::randomSlug(),
-                    $slug.'-'.Helpers::randomSlug()
-                ]
-            ]);
-        }
-        return $response->withJson(['status' => 302, 'message' => 'valid']);
     }
 }
