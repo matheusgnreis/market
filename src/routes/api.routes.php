5524207ce1316a25a4010acf07df42e45632ac4c
Talisson Ferreira <talissonf@gmail.com>
2019-02-06 23:59:39 -0200

    Request and response now is isolated from controllers

--- src/routes/api.routes.php
+++ src/routes/api.routes.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Market Api
+ * @todo Check if request has resourceId [middleware]
+ * @todo Check if required properties is set on request when is put/patch/post [middleware]
  */
 use Market\Controller\AppsController;
 
@@ -8,17 +10,41 @@ $app->group('/v1', function () use ($app) {
     /**
      * Applications resource
      */
-    $app->group('/applications', function () use ($app) {
-        $app->get('', AppsController::class . ':getAll');
-        $app->post('', AppsController::class . ':create');
-        $app->get('/{id}', AppsController::class . ':getById');
-        $app->patch('/{id}', AppsController::class . ':update');
-        $app->put('/{id}', AppsController::class . ':update');
-    });
+    $app->group(
+        '/applications',
+        function () use ($app) {
+            // controller
+            $applicationController = new AppsController();
 
-    /**
-     * Themes resource
-     */
-    /*     $app->group('/themes', function () use ($app) {
-    }); */
+            /**
+             * Request all applications
+             */
+            $app->get('', function ($request, $response) use ($applicationController) {
+                $params = $request->getParams();
+                return $response->withJson($applicationController->getAll($params));
+            });
+
+            /**
+             * Create new application
+             */
+            $app->post('', AppsController::class . ':create');
+
+            /**
+             * Request application by Id
+             */
+            $app->get('/{id}', function ($request, $response, $args) use ($applicationController) {
+                return $response->withJson($applicationController->getById($args['id']));
+            });
+
+            /**
+             * Patch Application
+             */
+            $app->patch('/{id}', AppsController::class . ':update');
+
+            /**
+             * Update Application
+             */
+            $app->put('/{id}', AppsController::class . ':update');
+        }
+    );
 });
