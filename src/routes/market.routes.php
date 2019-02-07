df387f0eff2183cfda16a49c32f08763d22e4b8b
Talisson Ferreira <talissonf@gmail.com>
2019-02-06 23:58:12 -0200

    Handle apps view

--- src/routes/market.routes.php
+++ src/routes/market.routes.php
@@ -1,13 +1,23 @@
 <?php
+/**
+ * Routes of public views
+ */
 use Market\Controller\HomeController;
 
 /**
  * Market views
  */
-$app->get('/', function () {
-    echo "redirect lang default.";
-})->add($redirectToLang);
+$app->get(
+    '/',
+    function () {
+        echo "redirect lang default.";
+    }
+)->add($redirectToLang);
 
-$app->group('/{lang}', function () use ($app, $redirectToLang) {
-    $app->get('', HomeController::class . ':home')->add($redirectToLang);
-});
+$app->group(
+    '/{lang}',
+    function () use ($app, $redirectToLang) {
+        $app->get('', HomeController::class . ':home')->add($redirectToLang);
+        $app->get('/apps', HomeController::class . ':apps')->add($redirectToLang);
+    }
+);
