97f6ca5f60827cd60f8e73a1d52f85c94473a2ae
Talisson Ferreira <talissonf@gmail.com>
2019-02-06 23:55:25 -0200

    Add: apps page view

--- src/Market/Controller/HomeController.php
+++ src/Market/Controller/HomeController.php
@@ -1,11 +1,20 @@
 <?php
 namespace Market\Controller;
+use Market\Controller\AppsController;
 
 class HomeController extends BaseController
 {
     use \Market\Services\Dictionary;
     use \Market\Services\Categories;
 
+    /**
+     * Home view
+     *
+     * @param [Object] $request
+     * @param [Object] $response
+     * @param [Object] $args
+     * @return void
+     */
     public function home($request, $response, $args)
     {
         $translate = $this->getDictionary($args['lang']);
@@ -22,5 +31,29 @@ class HomeController extends BaseController
             ],
         ];
         return $this->view->render($response, 'index.html', $params);
+    }
+
+    /**
+     * Apps View
+     */
+    public function apps($request, $response, $args)
+    {
+        $apps = new AppsController();
+        $all = $apps->getAll($request->getParams());
+        $translate = $this->getDictionary($args['lang']);
+        $params = [
+            'params' => [
+                'dictionary' => $translate,
+                'categories' => [
+                    'applications' => $this->appsCategories($translate),
+                    'themes' => $this->themesCategories($translate),
+                ],
+                'page' => [
+                    'name' => 'Apps',
+                ],
+                'data' => $all['result']
+            ],
+        ];
+        return $this->view->render($response, 'apps.html', $params);
     }
 }
