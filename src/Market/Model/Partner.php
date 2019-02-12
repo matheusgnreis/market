b0c8d5f51484ff0b42054dc7bd6e71af50ff4e88
Talisson Ferreira <talissonf@gmail.com>
2019-02-11 23:18:33 -0200

    Update: model property

--- src/Market/Model/Partner.php
+++ src/Market/Model/Partner.php
@@ -8,9 +8,9 @@ class Partner extends Model
     /** db table */
     protected $table = 'partners';
     /** The attributes that are mass assignable. */
-    protected $fillable = ['id', 'name', 'member_since', 'avg_stars', 'evaluations', 'path_image', 'profile_json', 'credit'];
+    protected $fillable = ['id', 'name', 'member_since', 'avg_stars', 'evaluations', 'path_image', 'profile_json'];
     /** The attributes that will be hidden */
-    protected $hidden = ['password_hash'];
+    protected $hidden = ['password_hash', 'credit'];
     /** Indicates if the model should be timestamped. */
     public $timestamps = false;
 
