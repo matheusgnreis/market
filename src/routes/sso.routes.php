9f1df03d65dc31becdb6579f3489c23db67fffa1
Talisson Ferreira <talissonf@gmail.com>
2019-02-13 16:01:06 -0200

    Add: create session routes

--- src/routes/sso.routes.php
+++ src/routes/sso.routes.php
@@ -7,3 +7,12 @@ $app->get(
         print_r($sso->handle_response());
     }
 );
+
+$app->get(
+    '/session/create',
+    function ($request, $response, $args) use ($app) {
+        var_dump($response);
+        $sso = new Market\Services\EcomSSO('EUsJDhFXwZ242mszKJDCkB3nbv7p69NT');
+        return $response->withRedirect($sso->login_url());
+    }
+);
