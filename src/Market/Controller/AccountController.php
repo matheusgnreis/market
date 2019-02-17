c9d7f192d6ffedfa9c981c8bdefa0510fe4589de
Talisson Ferreira <talissonf@gmail.com>
2019-02-17 08:25:47 -0300

    render account view

--- src/Market/Controller/AccountController.php
+++ src/Market/Controller/AccountController.php
@@ -10,9 +10,21 @@ class AccountController extends BaseController
 
     public function account($request, $response)
     {
+        if (!$_SESSION) {
+            session_start();
+        }
+        $partnerId = $_SESSION['id'];
+        $params = [
+            'params' => [
+                'data' => [
+                    'partner_id' => $partnerId,
+                ],
+            ],
+        ];
         return $this->view->render(
             $response,
-            'layout/account.html', []
+            'layout/account.html',
+            $params
         );
     }
 }
