86c6a494774c80cc3f7d8462926f62ff8c0c1faf
Talisson Ferreira <talissonf@gmail.com>
2019-02-17 22:56:55 -0300

    Sent categories from apps and themes to twig_view

--- src/Market/Controller/AccountController.php
+++ src/Market/Controller/AccountController.php
@@ -12,13 +12,23 @@ class AccountController extends BaseController
     {
         if (!$_SESSION) {
             session_start();
+            if (!$_SESSION['login']) {
+                return $response->withRedirect('./');
+            }
         }
+        $translate = $this->getDictionary('pt_br');
         $partnerId = $_SESSION['id'];
         $params = [
             'params' => [
                 'data' => [
                     'partner_id' => $partnerId,
                 ],
+                'dictionary' => $translate,
+                'categories' => [
+                    'applications' => $this->appsCategories($translate),
+                    'themes' => $this->themesCategories($translate),
+                ],
+                'resources' => $this->resources()
             ],
         ];
         return $this->view->render(
