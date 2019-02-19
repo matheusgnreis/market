9a439b816a573d1b45c5f94dff339d08a605fbb5
Talisson Ferreira <talissonf@gmail.com>
2019-02-18 23:24:35 -0300

    Add: Validate property before application are create

--- src/middleware.php
+++ src/middleware.php
@@ -1,4 +1,6 @@
 <?php
+use Respect\Validation\Validator as v;
+
 $redirectToLang = function ($request, $response, $next) {
     $uri = $request->getUri();
     $route = $request->getAttribute('route');
@@ -11,15 +13,49 @@ $redirectToLang = function ($request, $response, $next) {
         if ($lang[1] != 'pt_br' && $lang[1] != 'en_us') {
             return $response->withStatus(302)->withHeader('Location', '/pt_br' . $uri->getPath());
         }
-    } 
+    }
     return $next($request, $response);
 };
 
 $verifyLogin = function ($request, $response, $next) {
-    
+
     if (!\Market\Controller\LoginController::hasLogin()) {
         return $response->withStatus(302)->withHeader('Location', '/pt_br');
     }
-    
+
     return $next($request, $response);
 };
+
+/**
+ * Validate application post
+ */
+
+$applicationIsValid = array(
+    'partner_id' => v::intVal()->notEmpty(),
+    'title' => v::stringType()->notEmpty()->length(1, 40),
+    'slug' => v::slug()->notEmpty(),
+    'category' => v::stringType()->notEmpty(),
+    'icon' => v::stringType()->notEmpty(),
+    'description' => v::stringType(),
+    'short_description' => v::stringType()->notEmpty()->length(1, 300),
+    'json_body' => v::stringType(),
+    'paid' => v::boolVal()->notEmpty(),
+    'version' => v::version()->notEmpty(),
+    'version_date' => v::date(),
+    'type' => v::stringType(),
+    'module' => v::stringType(),
+    'load_events' => v::arrayVal(),
+    'script_uri' => v::url(),
+    'github_repository' => v::url(),
+    'authentication' => v::boolVal()->notEmpty(),
+    'auth_callback_uri' => v::url(),
+    'redirect_uri' => v::url(),
+    'auth_scope' => v::stringType(),
+    'avg_stars' => v::intVal(),
+    'evaluations' => v::intVal(),
+    'downloads' => v::intVal(),
+    'website' => v::url(),
+    'link_video' => v::url(),
+    'plans_json' => v::stringType(),
+    'value_plan_basic' => v::intVal(),
+);
