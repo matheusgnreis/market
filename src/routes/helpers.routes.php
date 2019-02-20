6ed972ed6c458cfb94cf7d39aadabc8d3c9f1cf1
Talisson Ferreira <talissonf@gmail.com>
2019-02-20 12:56:54 -0300

    Add: upload resource

--- src/routes/helpers.routes.php
+++ src/routes/helpers.routes.php
@@ -2,9 +2,14 @@
 /**
  * retorna adictionario
  */
+use Market\Services\FileUploader;
+use Market\Controller\MediaController;
+
 $app->get(
     '/dictionary/{lang}',
     function ($request, $response, $args) {
         return $response->withJson(\Market\Services\Dictionary::getDictionary($args['lang']));
     }
 );
+
+$app->post('/uploads', MediaController::class . ':create');
