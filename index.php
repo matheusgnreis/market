b9873852b5e033ce2ec5e14c3e1190f42be5e6e4
Talisson Ferreira <talissonf@gmail.com>
2019-02-24 21:55:48 -0300

    added admin routes

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ require __DIR__ . '/src/middleware.php';
 
 require __DIR__ . '/src/routes/api.routes.php';
 require __DIR__ . '/src/routes/account.routes.php';
+require __DIR__ . '/src/routes/admin.routes.php';
 require __DIR__ . '/src/routes/helpers.routes.php';
 require __DIR__ . '/src/routes/login.routes.php';
 require __DIR__ . '/src/routes/market.routes.php';
