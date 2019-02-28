2c6af14e0726bc014930d95d77ca4fef04c344ba
Talisson Ferreira <talissonf@gmail.com>
2019-02-27 22:45:08 -0300

    added routes for themes and components

--- src/routes/api.routes.php
+++ src/routes/api.routes.php
@@ -5,9 +5,11 @@
  * @todo Check if required properties is set on request when is put/patch/post [middleware]
  */
 use Market\Controller\AppsController;
+use Market\Controller\ComponentsController;
 use Market\Controller\PartnerController;
+use Market\Controller\ThemesController;
 
-$app->group('/v1', function () use ($app, $applicationIsValid) {
+$app->group('/v1', function () use ($app, $applicationIsValid, $componentsIsValid, $themeIsValid, $updateThemeIsValid) {
     /**
      * Applications resource
      */
@@ -68,6 +70,127 @@ $app->group('/v1', function () use ($app, $applicationIsValid) {
         }
     );
 
+    /**
+     * Applications resource
+     */
+    $app->group(
+        '/components',
+        function () use ($app, $componentsIsValid) {
+            // controller
+            $componentsController = new ComponentsController();
+
+            /**
+             * Request all components
+             */
+            $app->get('', function ($request, $response) use ($componentsController) {
+                $params = $request->getParams();
+                return $response->withJson($componentsController->getAll($params));
+            });
+
+            /**
+             * Create new components
+             */
+            $app->post('', function ($request, $response) use ($componentsController) {
+                if ($request->getAttribute('has_errors')) {
+                    //There are errors, read them
+                    $validateErrors = $request->getAttribute('errors');
+                    foreach ($validateErrors as $key => $value) {
+                        $errors[] = [
+                            'status' => 400,
+                            'property' => $key,
+                            'message' => 'Bad-formatted JSON {' . $key . '} invalid, details in user_message',
+                            'user_message' => str_replace('null ', '', implode(', ', $value)),
+                        ];
+                    }
+                    return $response->withJson(['erros' => $errors], 400);
+                } else {
+                    $body = $request->getParsedBody();
+                    $resp = $componentsController->create($body);
+
+                    return $response->withJson($resp, 201);
+                }
+            })->add(new \DavidePastore\Slim\Validation\Validation($componentsIsValid));
+
+            /**
+             * Request components by Id
+             */
+            $app->get('/{id}', function ($request, $response, $args) use ($componentsController) {
+                $component = $componentsController->getById($args['id']);
+                $result = $component ? $component : [];
+                return $response->withJson($result);
+            });
+
+            /**
+             * Patch components
+             */
+            $app->patch('/{id}', AppsController::class . ':update');
+
+            /**
+             * Update components
+             */
+            $app->put('/{id}', AppsController::class . ':update');
+        }
+    );
+
+    /**
+     * Theme resource
+     */
+    $app->group(
+        '/themes',
+        function () use ($app, $themeIsValid, $updateThemeIsValid) {
+            // controller
+            $themesController = new ThemesController();
+
+            /**
+             * Request all theme
+             */
+            $app->get('', function ($request, $response) use ($themesController) {
+                $params = $request->getParams();
+                return $response->withJson($themesController->getAll($params));
+            });
+
+            /**
+             * Create new theme
+             */
+            $app->post('', function ($request, $response) use ($themesController) {
+                if ($request->getAttribute('has_errors')) {
+                    //There are errors, read them
+                    $validateErrors = $request->getAttribute('errors');
+                    foreach ($validateErrors as $key => $value) {
+                        $errors[] = [
+                            'status' => 400,
+                            'property' => $key,
+                            'message' => 'Bad-formatted JSON {' . $key . '} invalid, details in user_message',
+                            'user_message' => str_replace('null ', '', implode(', ', $value)),
+                        ];
+                    }
+                    return $response->withJson(['erros' => $errors], 400);
+                } else {
+                    $body = $request->getParsedBody();
+                    $resp = $themesController->create($body);
+
+                    return $response->withJson($resp, 201);
+                }
+            })->add(new \DavidePastore\Slim\Validation\Validation($themeIsValid));
+
+            /**
+             * Request theme by Id
+             */
+            $app->get('/{id}', function ($request, $response, $args) use ($themesController) {
+                return $response->withJson($themesController->getById($args['id']));
+            });
+
+            /**
+             * Patch theme
+             */
+            $app->patch('/{id}', ThemesController::class . ':update')->add($updateThemeIsValid);
+
+            /**
+             * Update theme
+             */
+            $app->put('/{id}', ThemesController::class . ':update')->add($updateThemeIsValid);
+        }
+    );
     /**
      * Partner resource
      */
