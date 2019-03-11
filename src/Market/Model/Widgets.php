9b12b27910c8e91b00552fb4d33869ac43d4ab17
Talisson Ferreira <talissonf@gmail.com>
2019-03-11 09:35:31 -0300

    minor update

--- src/Market/Model/Widgets.php
+++ src/Market/Model/Widgets.php
@@ -8,9 +8,12 @@ class Widgets extends Model
     /** db table */
     protected $table = 'widgets';
     /** The attributes that are mass assignable. */
-    protected $fillable = ['id', 'partner_id', 'title', 'slug', 'css', 'html', 'ejs', 'js', 'json_schema', 'icon'];
+    protected $fillable = ['app_id', 'partner_id', 'title', 'slug', 'url_css', 'url_js', 'template', 'config', 'paid', 'icon'];
     /** The attributes that will be hidden */
     protected $hidden = [];
+    // primary key
+    protected $primaryKey = 'app_id';
+
     /** Indicates if the model should be timestamped. */
     public $timestamps = false;
 
