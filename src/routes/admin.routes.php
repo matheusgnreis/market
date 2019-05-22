67c289f17c23f06d7e81ac7885d94fdd9c6335ac
Talisson Ferreira <talissonf@gmail.com>
2019-05-22 12:27:11 -0300

    feat: edit app route

--- src/routes/admin.routes.php
+++ src/routes/admin.routes.php
@@ -3,5 +3,8 @@
  * Account routes
  */
 use Market\Controller\AdminController;
+
 //
 $app->get('/admin', AdminController::class . ':admin');
+$app->get('/admin/applications', AdminController::class . ':admin_applications');
+$app->get('/admin/applications/{application_id}/edit', AdminController::class . ':edit_application');
