e264255bc02d40f1166614d1228fcafb3854cd68
Talisson Ferreira <talissonf@gmail.com>
2019-06-10 02:27:51 -0300

    chore: minor update

--- src/dependencies.php
+++ src/dependencies.php
@@ -74,7 +74,7 @@ $container['errorHandler'] = function ($c) {
                 ->withHeader('Content-Type', 'application/json')
                 ->write(json_encode(array('erro' => array('code' => 500, 'message' => 'Não foi possível processar a solicitação.', 'Exception' => htmlspecialchars($exception)))));
         } else {
-            return $c['view']->render($response->withStatus(500), '500.html');
+            return $c['view']->render($response->withStatus(500), 'market/500.html');
         }
     };
 };
