bca70fcdde539ad3347f7706cbd58145d1c640e7
Talisson Ferreira <talissonf@gmail.com>
2019-06-10 00:59:22 -0300

    fix: open modal after login login

--- assets/js/src/script.js
+++ assets/js/src/script.js
@@ -1,14 +1,13 @@
 $(function () {
   var storeId = $('body').data('store')
   var storeSSO = $('body').data('sso')
-  var itemId = $('#app-resume').data('id')
+  var itemId = $('#main-single').data('id')
   var token = $('body').data('access')
   var myId = $('body').data('my-id')
-  var windowSsoLogin
   //
 
-  if($('body').data('sso')){
-    if($('body').data('sso') !== '' && $('body').data('sso') === 1){
+  if ($('body').data('sso')) {
+    if ($('body').data('sso') !== '' && $('body').data('sso') === 1) {
       $("#btn-login-logista").attr("href", "/admin");
       $("#btn-login-logista").text('Painel Lojista');
     }
@@ -18,128 +17,82 @@ $(function () {
     // if not sso login
     // request new
     if (storeSSO === '' || storeSSO === 0) {
-      window.sessionStorage.setItem('installItem', itemId)
+      console.log(storeSSO)
+      window.sessionStorage.setItem('installItem', $('#btn-install').data('id'))
       openWindow()
     } else {
-      // checks what item will install
-      var itemType = $(this).data('type')
+      // show application scope
+      $('#modal-scope-installation').modal('show')
+      // listen click in confirmation btn
+      $('#btn-confirmation').on('click', function (e) {
+        $.getJSON('/v1/applications/' + itemId)
+          .done(function (appData) {
+            console.log('Installing Application..', itemId)
+            // remove aditional data of application market data
+            // to sent for ecomplus application resource
+            var application = {}
 
-      switch (itemType) {
-        // if item is a widget, we need just storeId
-        // to register widget at market
-        case 'widgets':
-          // get widget data
-          $.getJSON('/v1/widgets/' + itemId)
-            .done(function (item) {
-              // logic for item paid
-              if (item.paid && item.paid === 1) {
-                // todo ...
+            application.app_id = appData.app_id
+            application.state = 'active'
+            application.title = appData.title
+            application.slug = appData.slug
+            application.paid = appData.paid
+            application.version = appData.version
+            application.version_date = new Date(appData.version_date).toISOString()
+            application.type = appData.type
+            application.modules = appData.module || {}
+            application.admin_settings = appData.json_body || {}
+            application.load_events = appData.load_events
+            application.script_uri = appData.script_uri
+            application.github = appData.github
+            application.status = 'active'
+            application.authentication = appData.authentication
+            application.auth_callback_uri = appData.auth_callback_uri
+            application.auth_scope = appData.auth_scope
+
+            //
+            /* application.module
+            application.data
+            application.hidden_data */
+            $.ajax({
+              type: 'POST',
+              url: 'https://api.e-com.plus/v1/applications.json',
+              headers: {
+                'X-Store-ID': storeId,
+                'X-Access-Token': token,
+                'X-My-Id': myId
+              },
+              data: JSON.stringify(application),
+              contentType: 'application/json',
+              dataType: 'json'
+            }).done(function (json) {
+              if (json._id) {
+                setTimeout(function () {
+                  if (typeof appData.redirect_uri !== 'undefined' && appData.redirect_uri !== null) {
+                    var url = appData.redirect_uri + '?x_store_id=' + storeId
+                    window.open(url, '_blank', 'location=yes,width=900,scrollbars=yes,status=yes')
+                  }
+                }, 1000)
+                // hide modal
+                $('#modal-scope-installation').modal('hide')
+                // show modal of configuration
+                var msg = 'Aplicativo instalado com sucesso. Você pode configurá-lo clicando abaixo.'
+                var link = '/admin/applications/' + json._id + '/edit'
+                successAlert(msg, link)
               }
-
-              var widget = {
-                store_id: storeId,
-                app_id: item.app_id
-              }
-
-              $.ajax({
-                type: 'POST',
-                url: '/v1/installations',
-                data: widget,
-                dataType: 'json'
-              })
-                .done(function (resp) {
-                  var msg = 'Widget instalado com sucesso. Você pode configurá-lo clicando abaixo.'
-                  var link = 'https://app.e-com.plus/admin/widget/' + resp._id
-                  successAlert(msg, link)
-                })
-                .fail(function (xhr) {
-                  console.log(xhr)
-                })
-            })
-            .fail(function (xhr) {
+            }).fail(function (xhr) {
               console.log(xhr)
             })
-
-          break
-        // if item is a application, we need to request login
-        // at ecomplus api to the installation in store-id
-        case 'apps':
-          // show application scope
-          $('#modal-scope-installation').modal('show')
-
-          // listen click in confirmation btn
-          $('#btn-confirmation').on('click', function (e) {
-            $.getJSON('/v1/applications/' + itemId)
-              .done(function (appData) {
-                console.log('Installing Application..', itemId)
-                // remove aditional data of application market data
-                // to sent for ecomplus application resource
-                var application = {}
-
-                application.app_id = appData.app_id
-                application.state = 'active'
-                application.title = appData.title
-                application.slug = appData.slug
-                application.paid = appData.paid
-                application.version = appData.version
-                application.version_date = new Date(appData.version_date).toISOString()
-                application.type = appData.type
-                application.modules = appData.module || {}
-                application.admin_settings = appData.json_body || {}
-                application.load_events = appData.load_events
-                application.script_uri = appData.script_uri
-                application.github = appData.github
-                application.status = 'active'
-                application.authentication = appData.authentication
-                application.auth_callback_uri = appData.auth_callback_uri
-                application.auth_scope = appData.auth_scope
-
-                //
-                /* application.module
-                application.data
-                application.hidden_data */
-                $.ajax({
-                  type: 'POST',
-                  url: 'https://api.e-com.plus/v1/applications.json',
-                  headers: {
-                    'X-Store-ID': storeId,
-                    'X-Access-Token': token,
-                    'X-My-Id': myId
-                  },
-                  data: JSON.stringify(application),
-                  contentType: 'application/json',
-                  dataType: 'json'
-                }).done(function (json) {
-                  if (json._id) {
-                    setTimeout(function () {
-                      if (typeof appData.redirect_uri !== 'undefined' && appData.redirect_uri !== null) {
-                        var url = appData.redirect_uri + '?x_store_id=' + storeId
-                        window.open(url, '_blank', 'location=yes,width=900,scrollbars=yes,status=yes')
-                      }
-                    }, 1000)
-                    // hide modal
-                    $('#modal-scope-installation').modal('hide')
-                    // show modal of configuration
-                    var msg = 'Aplicativo instalado com sucesso. Você pode configurá-lo clicando abaixo.'
-                    var link = '/admin/applications/' + json._id + '/edit'
-                    successAlert(msg, link)
-                  }
-                }).fail(function (xhr) {
-                  console.log(xhr)
-                })
-              })
           })
-          break
-        default: break
-      }
+      })
     }
   }
 
   if (window.sessionStorage.getItem('installItem') !== null) {
-    if (parseInt(window.sessionStorage.getItem('installItem')) === itemId) {
+    if (parseInt(window.sessionStorage.getItem('installItem')) === $('#btn-install').data('id')) {
       // show modal
       installItem()
-      // $('#modal-scope-installation').modal('show')
+       $('#modal-scope-installation').modal('show')
       // remove item
       window.sessionStorage.removeItem('installItem')
     }
