ce749c0930e3b3f95cb917a36b0d2dd6989fedf1
Talisson Ferreira <talissonf@gmail.com>
2019-06-10 02:10:44 -0300

    fix: checks if there is session before call route

--- src/routes/admin.routes.php
+++ src/routes/admin.routes.php
@@ -4,7 +4,18 @@
  */
 use Market\Controller\AdminController;
 
+$middleware = function ($request, $response, $next) {
+    if ($_SESSION) {
+        session_start();
+    }
+
+    if (!$_SESSION['sso_login'] || (isset($_SESSION['sso_login']) && $_SESSION['sso_login'] !== true)) {
+        return $response->withRedirect('/session/create');
+    }
+    $next();
+};
+
 //
-$app->get('/admin', AdminController::class . ':admin');
-$app->get('/admin/applications', AdminController::class . ':admin_applications');
-$app->get('/admin/applications/{application_id}/edit', AdminController::class . ':edit_application');
+$app->get('/admin', AdminController::class . ':admin')->add($middleware);
+$app->get('/admin/applications', AdminController::class . ':admin_applications')->add($middleware);
+$app->get('/admin/applications/{application_id}/edit', AdminController::class . ':edit_application')->add($middleware);
